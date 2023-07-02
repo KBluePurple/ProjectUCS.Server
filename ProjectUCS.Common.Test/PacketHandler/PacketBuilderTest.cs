c8df2fe2840504c41f06a33d070c3ea099e803bd
KBluePurple <42506968+BlWon@users.noreply.github.com>
2023-07-03 00:34:57 +0900

    패킷 빌더 테스트 코드 작성

---
+++ ProjectUCS.Common.Test/PacketHandler/PacketBuilderTest.cs
@@ -0,0 +1,33 @@
+﻿using ProjectUCS.Common.Data;
+using ProjectUCS.Common.Data.Serializer;
+
+namespace ProjectUCS.Common.Test;
+
+public class PacketBuilderTest
+{
+    private bool _completed;
+    
+    [Test]
+    public void BuildTest()
+    {
+        var packet = new C2S.ChatPacket
+        {
+            Message = "Test"
+        };
+
+        var serialized = PacketSerializer.Serialize(packet);
+        var builder = new PacketBuilder();
+        builder.Init(serialized.Length);
+
+        builder.OnCompleted += buffer =>
+        {
+            var root = PacketSerializer.Deserialize(buffer);
+            _completed = true;
+            Assert.That(root, Is.Not.Null);
+        };
+
+        for (var i = 0; i < serialized.Length; i++) builder.Append(serialized, i, 1);
+        
+        Assert.That(_completed, Is.True);
+    }
+}
