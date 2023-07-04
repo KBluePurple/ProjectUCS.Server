763e12de160a3cd954487429d1bc436728107d9f
KBluePurple <42506968+BlWon@users.noreply.github.com>
2023-07-04 10:54:55 +0900

    구조를 어떻게 추가할까 고민중

--- ProjectUCS.Server/Program.cs
+++ ProjectUCS.Server/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System.Reflection;
+using ProjectUCS.Common;
 using ProjectUCS.Common.Data;
 using ProjectUCS.Server.Core;
 using ProjectUCS.Server.Room;
 
 PacketHandlerManager.RegisterHandlers(Assembly.GetExecutingAssembly());
-
-var packet = new S2C.ChatPacket
-{
-    Message = "Hello World!"
-};
 
 var protocol = new Protocol(7777);
 protocol.Start();
@@ -18,10 +14,27 @@ var square = new BaseRoom(10);
 protocol.OnClientConnected += (_, connection) =>
 {
     Console.WriteLine("Client connected!");
-    connection.Send(packet);
     square.AddPlayer(connection);
 };
 
 protocol.OnClientDisconnected += (_, _) => { Console.WriteLine("Client disconnected!"); };
 
 await Task.Delay(-1);
+
+[RpcHandler(typeof(C2S.ChatPacket))]
+void OnChatPacket(Connection connection, C2S.ChatPacket packet)
+{
+    Console.WriteLine($"Message: {packet.Message}");
+    connection.Send(packet);
+}
+
+[AttributeUsage(AttributeTargets.Method)]
+public class RpcHandlerAttribute : Attribute
+{
+    public Type PacketType { get; }
+
+    public RpcHandlerAttribute(Type packetType)
+    {
+        PacketType = packetType;
+    }
+}
